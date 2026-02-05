6c44ad1d2b93cc26e1f80c147a58376bdb41c22c
aldesilv <alexander@nod-labs.com>
2026-02-05 06:43:16 -0800

    hi again

--- transliterate.py
+++ transliterate.py
@@ -1,15 +1,15 @@
 def transliterate(a):
     a = a.lower()
-    if a == "kill yourself":
-        return "love yourself"
-    elif a == "you're dead":
-        return "you're healthy"
+    if a == "kill yourself.":
+        return "love yourself."
+    elif a == "you're dead.":
+        return "you're healthy."
     elif len(a) >= 8 and a[3:8] == "hates":
-        return "he loves" + a [8:]
-    elif a == "kill yourself":
+        return "he loves" + a [8:] + "."
+    elif a == "kill yourself.":
         return "love yourself"
-    elif a == "why don't you just suicide":
-        return "why  don't you just live"
+    elif a == "why don't you just suicide.":
+        return "why  don't you just live."
     return ""
 
 if __name__ == "__main__":
